9ee5410c42542bea41746cac03a6f79f26812146
jsmjsm <levy-pan@outlook.com>
2021-05-18 13:39:28 +0000

    hardhat & slither

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Description  :
+ * @Author       : jsmjsm
+ * @Github       : https://github.com/jsmjsm
+ * @Date         : 2021-05-18 08:21:49
+ * @LastEditors  : jsmjsm
+ * @LastEditTime : 2021-05-18 08:22:07
+ * @FilePath     : /FixedSupplyToken/hardhat.config.js
+ */
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+	solidity: '0.4.24',
+}
